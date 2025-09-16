b1490eb822efe1959dd6ecf949b8933b2425c5c9
Syed Musharraf Ali <syed@bobbie.de>
2025-09-16 16:03:28 +0200

    final code changes before dvc

---
+++ pipeline/training_pipeline.py
@@ -0,0 +1,17 @@
+from utils.common_functions import read_yaml
+
+from config.path_config import *
+from src.data_ingestion import DataIngestion
+from src.data_processing import DataProcessor
+from src.model_training import ModelTraining
+
+if __name__=="__main__":
+    # not using because we already have raw from GCP
+    # data_ingestion = DataIngestion(read_yaml(CONFIG_PATH))
+    # data_ingestion.run()
+
+    data_processor = DataProcessor(ANIMELIST_CSV,PROCESSED_DIR)
+    data_processor.run()
+
+    model_trainer = ModelTraining(PROCESSED_DIR)
+    model_trainer.train_model()
